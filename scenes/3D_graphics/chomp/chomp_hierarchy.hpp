2bea81730dea174c497a371b1a14c50ef55bbf78
alexandrethiault <alexandre.thiault@hotmail.com>
2020-04-23 19:23:13 +0200

    Hierarchie du chomp finie

--- scenes/3D_graphics/chomp/chomp_hierarchy.hpp
+++ scenes/3D_graphics/chomp/chomp_hierarchy.hpp
@@ -8,6 +8,18 @@ struct gui_scene_structure
 {
     bool wireframe = false;
     bool surface = true;
+    bool billboards = true;
+};
+
+struct chomp_structure
+{
+    float radius_chomp = 0.;
+    vcl::hierarchy_mesh_drawable hierarchy;
+    GLuint texture_up, texture_down, texture_tongue, texture_eye, texture_tooth, texture_chain;
+
+    void init();
+    void draw_nobillboards(std::map<std::string, GLuint>& shaders, scene_structure& scene, bool surf, bool wf);
+    void draw_billboards(std::map<std::string, GLuint>& shaders, scene_structure& scene, bool bb, bool wf);
 };
 
 struct scene_model : scene_base
@@ -17,8 +29,7 @@ struct scene_model : scene_base
     void frame_draw(std::map<std::string, GLuint>& shaders, scene_structure& scene, gui_structure& gui);
     void set_gui();
 
-    vcl::hierarchy_mesh_drawable hierarchy_chomp;
-
+    chomp_structure chomp;
     vcl::mesh_drawable demo_ground;
 
     gui_scene_structure gui_scene;
